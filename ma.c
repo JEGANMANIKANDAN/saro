405c8ad80e17eb24765c81669ea69520d66ad769
JEGANMANIKANDAN <35652628+JEGANMANIKANDAN@users.noreply.github.com>
2018-01-30 16:49:37 +0530

    Create ma.c

---
+++ ma.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void  main() 
+{
+    int n,m,i;
+    printf("enter the value of n");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        m+=i;
+      printf("%d",m);
+      }
+getch();
+}
